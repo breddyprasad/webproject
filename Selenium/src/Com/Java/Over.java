ddcc61cdd7cc6c48f4c75ac581db1f090d39c7ec
breddyprasad <prasad57199@gmail.com>
2017-04-28 11:32:59 +0530

    this selenium

---
+++ Selenium/src/Com/Java/Over.java
@@ -0,0 +1,22 @@
+package Com.Java;
+
+
+
+public class Over {
+	
+	public static void main(String[] args) {
+		
+		//String
+		
+		//Array a[] = new Array[100] ;
+		
+		System.out.println();
+		System.out.println("reddy");
+		System.out.println(30);
+		System.out.println('a');
+		System.out.println(12.2);
+		
+		
+	}
+
+}
---
+++ Selenium/src/Com/Java/Vari.java
@@ -0,0 +1,27 @@
+package Com.Java;
+
+public class Vari {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		for (int a = 0; a <=10; a++) {
+			
+		
+		
+		if (a%2!=0) {
+			
+			System.out.println(a);
+			
+		}
+		
+		}
+		
+	}
+	
+	
+
+	}
+
+
